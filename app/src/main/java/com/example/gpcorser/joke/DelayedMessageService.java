6ac3db8803714510b6abcab4fc0cebaf9bee17a0
gpcorser <gpcorser@svsu.edu>
2016-10-24 15:06:07 -0400

    Initial commit - displays Toast Message

---
+++ app/src/main/java/com/example/gpcorser/joke/DelayedMessageService.java
@@ -0,0 +1,45 @@
+package com.example.gpcorser.joke;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DelayedMessageService extends IntentService {
+    public static final String EXTRA_MESSAGE = "message";
+    private Handler handler;
+
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        synchronized (this) {
+            try {
+                wait(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
+    }
+
+    private void showText(final String text) {
+        // Log.v("delayedMessageService","The message is: " + text);
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
