0f6f6787a350d126d819124df8f656575d9b004d
gpcorser <gpcorser@svsu.edu>
2016-10-24 16:09:28 -0400

    added WombleService

--- app/src/main/java/com/example/gpcorser/joke/MainActivity.java
+++ app/src/main/java/com/example/gpcorser/joke/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Boolean isOn = false;
+    public Intent intentOn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,18 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
                 getResources().getString(R.string.button_response));
         startService(intent);
+    }
+
+    public void onClickWomble(View view) {
+        if (!isOn) {
+            Intent intent = new Intent(this, WombleService.class);
+            startService(intent);
+            intentOn = intent;
+            isOn = true;
+        } else {
+            stopService(new Intent(MainActivity.this, WombleService.class));
+            isOn = false;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/gpcorser/joke/WombleService.java
@@ -0,0 +1,35 @@
+package com.example.gpcorser.joke;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.media.MediaPlayer;
+
+
+public class WombleService extends IntentService {
+
+    public MediaPlayer mp;
+
+    public WombleService() {
+        super("WombleService");
+    }
+
+    @Override
+    protected void onHandleIntent (Intent intent) {
+        MediaPlayer mediaPlayer =
+                MediaPlayer.create(getApplicationContext(), R.raw.wombling_song);
+        mediaPlayer.start();
+        mp = mediaPlayer;
+    }
+
+    public void onDestroy() {
+        synchronized (this) {
+            try {
+                wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        mp.stop();
+    }
+}
